84836f3a4cd3ca0e39f2f316e606b8830d0425b4
Abijah Kajabika <migisho@yandex.ru>
2019-09-08 02:58:00 +0300

    Original

--- application.py
+++ application.py
@@ -7,25 +7,19 @@ from flask import request
 from flask import redirect
 from flask import render_template
 from flask import jsonify
-from flaskext.mysql import MySQL
-import hashlib
+
 
 import requests
 
 
 import pywaves as pw
 
-mysql = MySQL()
+
 
 # app = Flask(__name__) # to make the app run without any
 app = Flask(__name__)
 
-# MySQL Configuration
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mysql'
-app.config['MYSQL_DATABASE_DB'] = 'EmpData'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+
 
 # SERV
 @app.route('/', methods=['GET', 'POST'])
@@ -65,48 +59,6 @@ def not_found(error=None):
     resp.status_code = 404
 
     return resp
-
-@app.route('/users/<userid>', methods = ['GET'])
-def api_users(userid):
-    users = {'1':'john', '2':'steve', '3':'bill'}
-    
-    if userid in users:
-        return jsonify({userid:users[userid]})
-    else:
-        return not_found()
-
-
-
-@app.route("/login", methods = ['POST'])
-def Authenticate():
-    username = request.args.get('email')
-    password = request.args.get('password')
-    if username and password:
-        password = hashlib.md5(password.encode()).hexdigest()
-        cursor = mysql.connect().cursor()
-        cursor.execute("SELECT * from user where email='" + username + "' and pass='" + password + "'")
-        data = cursor.fetchone()
-        if data is None:
-         return "Username or Password is wrong"
-    else:
-     return "Logged in successfully"
-
-
-@app.route("/signup", methods = ['POST'])
-def Sigunp():
-    username = request.args.get('email')
-    address = request.args.get('adress')
-    password = request.args.get('password')
-    if username and address and password:
-        password = hashlib.md5(password.encode()).hexdigest()
-        cursor = mysql.connect().cursor()
-        cursor.execute("INSERT INTO user VALUES(NULL,'" + username + "','" + address + "','" + password + "',NOW()")
-        mysql.connect().commit()
-    else:
-        return jsonify({'error':'fill all the form' + username  })
-
-
-
 
 
 
