347cbca1589acab51825573d29cc650c4d9c0bd8
manaleks <manaleksdev@gmail.com>
2019-09-07 21:01:28 +0300

    test pywaves

--- application.py
+++ application.py
@@ -8,9 +8,10 @@ from flask import redirect
 from flask import render_template
 
 import requests
-import pywaves
 
-#transacton
+
+import pywaves as pw
+
 
 app = Flask(__name__)
 
@@ -21,6 +22,26 @@ def server_work():
     if request.method == 'POST':
         pass
     return render_template('main.html')
+
+@app.route('/waves', methods=['GET', 'POST'])
+def server_work():
+    if request.method == 'POST':
+        pass
+
+    myAddress = pw.Address(privateKey='4GXSoZdGi8bgkC98H8FpNZ5kjv92s8YASRsbNWXU7hVt')
+    print(1)
+    otherAddress = pw.Address('3NCcsyMn1R9feUBBnFCTBaVcWD2LKq4ErfG')
+    print(2)
+    myAddress.sendWaves(otherAddress, 10000)
+    print(3)
+    myToken = myAddress.issueAsset('Token1', 'My Token', 1000, 0)
+    print(4)
+    while not myToken.status():
+        pass
+    print(5)
+    myAddress.sendAsset(otherAddress, myToken, 50)
+
+    return 'ok'
 
 
 if __name__ == "__main__":
