c9cb29a4413ea1718440dd699547e5af230a8951
Abijah Kajabika <migisho@yandex.ru>
2019-09-08 15:56:15 +0300

    Fixed

--- static/js/script.js
+++ static/js/script.js
@@ -92,4 +92,37 @@ $(() => {
                 });
         event.preventDefault();
     });
+       $('#_deal').submit(event => {
+
+        event.preventDefault();
+        WavesKeeper.signAndPublishTransaction({
+            type: 16,
+            data: {
+                fee: {
+                    "tokens": "0.05",
+                    "assetId": "WAVES"
+                },
+                dApp: '3MvqUEAdK8oa1jDS82eqYYVoHTX3S71rRPa',
+                call: {
+                    function: 'main',
+                    args: [
+
+                        {
+                            "type": "string",
+                            "value": "home1"
+                        },
+                        {
+                            "type": "string",
+                            "value": "3MyhYfrLVNe5cj191rJHxqCrR5CU7URCN8h"
+                        }
+                    ]
+                },
+                payment: [{assetId: "WAVES", tokens: 2}]
+            }
+        }).then((tx) => {
+            console.log("Ура! Я выполнил скрипт!!!");
+        }).catch((error) => {
+            console.error("Что-то пошло не так", error);
+        });
+    });
 })
